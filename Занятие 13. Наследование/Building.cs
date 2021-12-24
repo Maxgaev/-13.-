1225c0ba038a80f9a051b8e1447293c45742dab9
Максим Алехин <Max-crf@mail.ru>
2021-12-24 15:24:01 +0700

    Добавьте файлы проекта.

---
+++ Занятие 13. Наследование/Building.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_13.Наследование
+{
+    internal class Building
+    {
+        public string Address { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Building(string address, double length, double width, double height)
+        {
+            Address = address;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Адрес: {Address}\nДлина: {Length}\nШирина: {Width}\nВысота: {Height}");
+        }
+    }
+    sealed class MultiBuilding : Building
+    {
+        public int NumberOfFloors { get; set; }
+
+        public MultiBuilding(string address, double length, double width, double height, int numberOfFloors)
+            : base(address, length, width, height)
+        {
+            NumberOfFloors = numberOfFloors;
+        }
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Количество этажей: {NumberOfFloors}");
+        }
+    }
+}
---
+++ Занятие 13. Наследование/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Занятие_13.Наследование
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные по зданию:");
+            Console.Write("\nАдрес: ");
+            string address = Console.ReadLine();
+            Console.Write("Длина: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ширина: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Высота: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Количество этажей: ");
+            int numberOfFloors = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Building b = new Building(address, length, width, height);
+            Console.Write("Вывод данных класса Building: \n");
+            b.Print();
+            Console.WriteLine();
+
+            MultiBuilding mb = new MultiBuilding(address, length, width, height, numberOfFloors);
+            Console.Write("Вывод данных класса MultiBuilding: \n");
+            mb.Print();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
